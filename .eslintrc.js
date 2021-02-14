da9f1417a0c2b04e2cd7452e60db5dfd03d28a4b
Rishabh Jain <rishabh@codeation.io>
2021-02-14 17:54:11 +0530

    fix: changed ecmascript version to 2021

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true
