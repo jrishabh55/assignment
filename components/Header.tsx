2ba6d3cd5b96f7f39a20f22c0ab2515927e48c7f
Rishabh Jain <rishabh@codeation.io>
2021-02-14 18:27:54 +0530

    feat: Header component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <>
+      <header className="border-b-2 h-16 flex items-center p-2">Header</header>
+    </>
+  );
+};
+
+export default Header;
