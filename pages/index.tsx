362a957d85f49d3f62c513e9d6bc82f2e7a8a27a
Rishabh Jain <rishabh@codeation.io>
2021-02-14 18:30:34 +0530

    feat: login component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
+import Login from 'components/Login';
+import Layout from 'components/Layout';
+import Footer from 'components/Footer';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>Skuad Assignment</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
-
-      <footer className={styles.footer}>Footer</footer>
-    </div>
+      <Login />
+    </Layout>
   );
 }
