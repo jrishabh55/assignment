d6225c0dde11fb7da90619bb170f256b2a3eed16
Rishabh Jain <rishabh@codeation.io>
2021-02-14 18:29:26 +0530

    feat: layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import Header from 'components/Header';
+import Meta from 'components/Meta';
+import { FC } from 'react';
+import Footer from './Footer';
+
+const Layout: FC = ({ children }) => {
+  return (
+    <>
+      <div className="flex flex-col w-screen h-screen">
+        <Meta />
+        <Header />
+        <main className="flex flex-grow">{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
