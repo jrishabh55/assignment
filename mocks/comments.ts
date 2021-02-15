fac56ef6a3656c3369b79d0188a181b28d760b1e
Rishabh Jain <rishabh@codeation.io>
2021-02-15 15:49:50 +0530

    chore: added mocks

---
+++ mocks/comments.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    author: {
+      name: 'Author 1'
+    },
+    discussion_id: 1,
+    comment_id: 1,
+    comment:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia soluta minus doloremque assumenda deserunt consequuntur necessitatibus placeat officiis unde, repellendus nemo quisquam asperiores adipisci, pariatur cum ratione atque. Tenetur, dolore?',
+    timestamp: '2015-12-01'
+  }
+];
