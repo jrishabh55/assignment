85caa1bd1f4265cfbf419d46f77534a8600239fa
Rishabh Jain <rishabh@codeation.io>
2021-02-15 18:02:47 +0530

    fix: added CenterContainer

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,8 @@ export default function Home(): ReactNode {
   const router = useRouter();
   return (
     <>
-      <CenterContainer>
-        <div className="flex flex-col space-y-3 w-48">
+      <CenterContainer className="bg-transparent">
+        <div className="flex flex-col space-y-3 w-48 bg-transparent">
           <Button variant="success" onClick={() => router.push('/login')}>
             Login
           </Button>
