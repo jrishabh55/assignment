bf5fd39afc96249da8dd50d674183f5ea5d5f49b
Rishabh Jain <rishabh@codeation.io>
2021-02-15 13:22:44 +0530

    feat: inhanced input component for size

--- components/Form/Input.tsx
+++ components/Form/Input.tsx
@@ -4,21 +4,32 @@ import { DetailedHTMLProps, FC, InputHTMLAttributes } from 'react';
 export interface InputProps
   extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   block?: boolean;
+  variant?: 'lg' | 'md' | 'sm';
 }
 
-const Input: FC<InputProps> = ({ className, block = false, ...props }) => {
+const Input: FC<InputProps> = ({ className, block, variant, ...props }) => {
   return (
     <>
       <input
         className={cx(
-          'p-4 shadow focus:outline-none border rounded-lg focus:border-gray-400 w-full',
-          { block },
+          'shadow focus:outline-none border rounded-lg focus:border-gray-400 w-full',
+          {
+            block,
+            ['p-4']: variant === 'lg',
+            ['p-2']: variant === 'md',
+            ['p-1']: variant === 'sm'
+          },
           className
         )}
         {...props}
       />
     </>
   );
+};
+
+Input.defaultProps = {
+  variant: 'lg',
+  block: false
 };
 
 export default Input;
