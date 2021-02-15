741efea42f972c1e667bf0548a1dfb23184cb52e
Rishabh Jain <rishabh@codeation.io>
2021-02-15 12:03:39 +0530

    fix: added home link to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react';
+import Link from 'next/link';
 
 const Header: FC = () => {
   return (
     <>
-      <header className="border-b-2 h-16 flex items-center p-2">Header</header>
+      <header className="border-b-2 h-16 flex items-center p-2">
+        <Link href="/">Skuad Reddit</Link>
+      </header>
     </>
   );
 };
