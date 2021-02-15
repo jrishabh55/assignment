73a9992e0646ec17d263fcf23a7e0b515185e05a
Rishabh Jain <rishabh@codeation.io>
2021-02-15 16:02:02 +0530

    feat: added link to the boards

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,23 +1,26 @@
 import Article, { ArticleBody, ArticleFooter, ArticleHeader } from 'components/Article';
 import { boards } from 'mocks/board';
+import Link from 'next/link';
 import React, { FC } from 'react';
 
 const Dashboard: FC = () => {
   return (
     <section className="mt-4 mx-auto p-2 rounded max-w-screen-md space-y-3 bg-transparent">
       {boards.map((board) => (
-        <Article key={board.id} className="max-h-48 h-48">
-          <ArticleHeader className="border border-b-0 shadow-md rounded-none panel-header p-2">
-            {board.title}
-          </ArticleHeader>
-          <ArticleBody className="whitespace-nowrap overflow-hidden overflow-ellipsis">
-            {board.description}
-          </ArticleBody>
-          <ArticleFooter className="grid grid-cols-2 divide-x divide-gray-500">
-            <span>Discussions: {board.discussions.length}</span>
-            <span>Owner: {board.author.username}</span>
-          </ArticleFooter>
-        </Article>
+        <Link key={board.id} href={`/boards/${board.slug}`}>
+          <Article className="max-h-48 h-48 cursor-pointer">
+            <ArticleHeader className="border border-b-0 shadow-md rounded-none panel-header p-2">
+              {board.title}
+            </ArticleHeader>
+            <ArticleBody className="whitespace-nowrap overflow-hidden overflow-ellipsis">
+              {board.description}
+            </ArticleBody>
+            <ArticleFooter className="grid grid-cols-2 divide-x divide-gray-500">
+              <span>Discussions: {board.discussions.length}</span>
+              <span>Owner: {board.author.username}</span>
+            </ArticleFooter>
+          </Article>
+        </Link>
       ))}
     </section>
   );
