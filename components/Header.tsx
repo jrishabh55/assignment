712fb5f0a50f18c0262293e2c15db92fd806490a
Rishabh Jain <rishabh@codeation.io>
2021-02-15 13:23:06 +0530

    feat: added search component support for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,22 @@
+import Input from 'components/Form/Input';
 import Link from 'next/link';
 import { FC } from 'react';
 
-const Header: FC = () => {
+const Header: FC<{ showSearch?: boolean }> = ({ showSearch }) => {
   return (
     <>
-      <header className="border-b-2 h-16 flex items-center p-2">
+      <header className="border-b-2 min:h-16 flex items-center p-2 overflow-hidden">
         <Link href="/">Skuad Reddit</Link>
+        {showSearch && (
+          <div className="w-96 ml-auto">
+            <Input id="search" type="search" placeholder="Search" variant="md" />
+          </div>
+        )}
       </header>
     </>
   );
 };
+
+Header.defaultProps = { showSearch: false };
 
 export default Header;
