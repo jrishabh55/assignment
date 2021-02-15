14b049ac0436a31dd6737c95016fd7b21c64ed4b
Rishabh Jain <rishabh@codeation.io>
2021-02-15 16:01:52 +0530

    chore: removed extras

--- mocks/board.ts
+++ mocks/board.ts
@@ -2,6 +2,7 @@ export const boards = [
   {
     id: 1,
     title: 'Board 1',
+    slug: 'board-1',
     author: {
       username: 'rishabh'
     },
@@ -10,46 +11,6 @@ export const boards = [
     discussions: [
       {
         id: 1,
-        title: 'Discussion 1',
-        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis voluptatibus ut cupiditate harum accusamus vel quis illum. Nisi, natus. Inventore ipsum rerum quasi porro voluptatibus aliquam ea culpa adipisci quidem?
-      Nobis aut necessitatibus nisi sit repudiandae facilis dolorem rem. Ratione tenetur molestiae, iste, est totam corrupti quaerat perferendis labore numquam dolores accusamus doloremque eaque id! Soluta ex ea eligendi voluptatum?
-      Quis, voluptates quisquam vitae modi voluptate earum. Praesentium velit similique ducimus. Porro nemo aspernatur consequuntur omnis provident at! Et soluta ipsa numquam impedit voluptates officiis ipsum omnis dolorem, nulla optio.
-      Neque labore rerum distinctio at. Adipisci harum perspiciatis nulla debitis eligendi assumenda repudiandae aspernatur exercitationem cumque dicta illum qui eaque, nobis dolorum hic repellat nemo? Non minus itaque ducimus quo?
-      Illum neque eveniet tempore ex magnam! Sint aliquid alias aperiam ab. Accusamus recusandae dignissimos vero at architecto ipsam? Qui eum aliquam ipsam possimus mollitia molestiae minus distinctio, expedita natus voluptatem.`
-      }
-    ]
-  },
-  {
-    id: 2,
-    title: 'Board 2',
-    author: {
-      username: 'rishabh'
-    },
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum nobis magnam voluptates eaque minus officia, debitis iste nostrum itaque? Deserunt error dolor recusandae, vitae asperiores obcaecati culpa placeat fugit labore?',
-    discussions: [
-      {
-        id: 2,
-        title: 'Discussion 1',
-        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis voluptatibus ut cupiditate harum accusamus vel quis illum. Nisi, natus. Inventore ipsum rerum quasi porro voluptatibus aliquam ea culpa adipisci quidem?
-      Nobis aut necessitatibus nisi sit repudiandae facilis dolorem rem. Ratione tenetur molestiae, iste, est totam corrupti quaerat perferendis labore numquam dolores accusamus doloremque eaque id! Soluta ex ea eligendi voluptatum?
-      Quis, voluptates quisquam vitae modi voluptate earum. Praesentium velit similique ducimus. Porro nemo aspernatur consequuntur omnis provident at! Et soluta ipsa numquam impedit voluptates officiis ipsum omnis dolorem, nulla optio.
-      Neque labore rerum distinctio at. Adipisci harum perspiciatis nulla debitis eligendi assumenda repudiandae aspernatur exercitationem cumque dicta illum qui eaque, nobis dolorum hic repellat nemo? Non minus itaque ducimus quo?
-      Illum neque eveniet tempore ex magnam! Sint aliquid alias aperiam ab. Accusamus recusandae dignissimos vero at architecto ipsam? Qui eum aliquam ipsam possimus mollitia molestiae minus distinctio, expedita natus voluptatem.`
-      }
-    ]
-  },
-  {
-    id: 3,
-    title: 'Board 3',
-    author: {
-      username: 'rishabh'
-    },
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum nobis magnam voluptates eaque minus officia, debitis iste nostrum itaque? Deserunt error dolor recusandae, vitae asperiores obcaecati culpa placeat fugit labore?',
-    discussions: [
-      {
-        id: 3,
         title: 'Discussion 1',
         description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis voluptatibus ut cupiditate harum accusamus vel quis illum. Nisi, natus. Inventore ipsum rerum quasi porro voluptatibus aliquam ea culpa adipisci quidem?
       Nobis aut necessitatibus nisi sit repudiandae facilis dolorem rem. Ratione tenetur molestiae, iste, est totam corrupti quaerat perferendis labore numquam dolores accusamus doloremque eaque id! Soluta ex ea eligendi voluptatum?
