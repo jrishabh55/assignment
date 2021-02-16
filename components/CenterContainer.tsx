9e16b30e235df2cd031c57237f83d33544b73374
Rishabh Jain <rishabh@codeation.io>
2021-02-16 19:09:24 +0530

    fix: changed center container bg

--- components/CenterContainer.tsx
+++ components/CenterContainer.tsx
@@ -4,6 +4,10 @@ import { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
 const CenterContainer: FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = ({
   children,
   className
-}) => <div className={cx('align-middle self-center mx-auto -mt-48', className)}>{children}</div>;
+}) => (
+  <div className={cx('align-middle self-center bg-transparent mx-auto -mt-48', className)}>
+    {children}
+  </div>
+);
 
 export default CenterContainer;
