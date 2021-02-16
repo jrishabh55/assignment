047152b6667a7395d601aeefa6ff84ee08da32fb
Rishabh Jain <rishabh@codeation.io>
2021-02-16 21:56:57 +0530

    feat: added footer margin top

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const Footer: FC = () => {
   return (
     <>
-      <footer className="border-t-2 p-2 flex-center text-center ">
+      <footer className="border-t-2 mt-2 p-2 flex-center text-center ">
         Built with &hearts; using Next.Js, TailwindCSS and FaunaDB. <br /> Deployed on Vercel By
         Rishabh Jain.
       </footer>
