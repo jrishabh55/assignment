2443198ed6d346be0d859ba57f059b180dd64644
Rishabh Jain <rishabh@codeation.io>
2021-02-16 21:57:07 +0530

    feat: comment api

--- pages/discussions/[slug].tsx
+++ pages/discussions/[slug].tsx
@@ -3,23 +3,47 @@ import Button from 'components/Button';
 import TextArea from 'components/Form/TextArea';
 import { stateContext, types } from 'context/stateContext';
 import { useRouter } from 'next/router';
-import React, { FC, useContext, useEffect } from 'react';
+import React, { ChangeEvent, FC, useCallback, useContext, useEffect, useState } from 'react';
 import { api } from 'utils/api';
 
 const Discussion: FC = () => {
+  const [comment, setComment] = useState('');
   const [{ threads, comments }, dispatch] = useContext(stateContext);
   const router = useRouter();
   const { slug } = router.query;
   const thread = threads.find((it) => it.data.slug === slug);
 
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
+
+  const fetchComments = useCallback(
+    () =>
+      api(`thread/${thread?.ref?.['@ref'].id}/comments`).then(({ comments }) => {
+        dispatch({ type: types.SET_COMMENTS, payload: comments });
+      }),
+    [dispatch, thread]
+  );
+
+  const saveComment = (e: any) => {
+    e.preventDefault();
+
+    api(`thread/${thread?.ref?.['@ref'].id}/comments`, {
+      method: 'POST',
+      body: { comment } as any
+    }).then(() => {
+      fetchComments();
+      setComment('');
+    });
+  };
+
   useEffect(() => {
     if (!thread?.ref?.['@ref'].id) {
       router.replace('/dashboard');
     }
-    api(`thread/${thread?.ref?.['@ref'].id}/comments`).then(({ comments }) => {
-      dispatch({ type: types.SET_COMMENTS, payload: comments });
-    });
-  }, [dispatch, thread, router]);
+    fetchComments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -39,16 +63,23 @@ const Discussion: FC = () => {
               <ArticleHeader className="grid grid-cols-2 divide-x divide-gray-500">
                 <div>By: {comment?.author?.username}</div>
                 <div className="flex pr-4 text-sm items-center justify-end">
-                  Date: {comment.timestamp}
+                  Date: {new Date(comment.timestamp).toLocaleString()}
                 </div>
               </ArticleHeader>
-              <ArticleBody className="text-sm">{comment.description}</ArticleBody>
+              <ArticleBody className="text-sm">{comment.comment}</ArticleBody>
             </Article>
           ))}
         </section>
         <section className="flex flex-col">
-          <TextArea className="mt-4" placeholder="Enter your comment here."></TextArea>
-          <Button className="p-1 mt-2 mr-2 w-2/12 self-end">Comment</Button>
+          <TextArea className="mt-4" placeholder="Enter your comment here." onChange={handleChange}>
+            {comment}
+          </TextArea>
+          <Button
+            className="p-1 mt-2 mr-2 w-2/12 self-end"
+            disabled={comment.length <= 5}
+            onClick={saveComment}>
+            Comment
+          </Button>
         </section>
       </section>
     </>
